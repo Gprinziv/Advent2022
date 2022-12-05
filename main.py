1384f57cd1884d29d2981ca71f35271537abe4c1
Gprinziv <gprinziv@gmail.com>
2022-12-06 00:12:24 +0900

    Refactoring because why not?

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from re import split as resplit
 
-def getStartingLists(start):
-    start = start.split("\n")
-    length = len(start[0]) + 1
-    parsedList = []
-    i = 0
-    while i < length/4:
-        temp = []
-        for line in start[:-1]:
-            if line[4*i + 1] != " ":
-                temp.insert(0, line[4*i+1])
-        parsedList.append(temp)
-        i += 1
-    return parsedList
-
-def p1():
+def getStartingLists():
     with open("input") as f:
         start, moves = f.read().split("\n\n")
     
@@ -22,22 +8,27 @@ def p1():
     for line in moves.split("\n"):
         nums = [int(i) for i in resplit("[a-z ]+", line)[1:]]
         instructions.append(nums)
-    boxes = getStartingLists(start)
 
+    boxes = []
+    start = start.split("\n")
+    for i in range(1, len(start[0]))[::4]:
+        temp = []
+        for line in start[:-1]:
+            if line[i] != " ":
+                temp.insert(0, line[i])
+        boxes.append(temp)
+    return instructions, boxes
+
+def p1():
+    instructions, boxes = getStartingLists()
+    
     for amt, toBox, fromBox in instructions:
         boxes[fromBox-1] += reversed(boxes[toBox-1][-amt:])
         boxes[toBox-1] = boxes[toBox-1][:-amt]
     return "".join(i[-1] for i in boxes)
 
 def p2():
-    with open("input") as f:
-        start, moves = f.read().split("\n\n")
-    
-    instructions = []
-    for line in moves.split("\n"):
-        nums = [int(i) for i in resplit("[a-z ]+", line)[1:]]
-        instructions.append(nums)
-    boxes = getStartingLists(start)
+    instructions, boxes = getStartingLists()
 
     for amt, toBox, fromBox in instructions:
         boxes[fromBox-1] += boxes[toBox-1][-amt:]
