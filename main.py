076e1a9b2d0d030cba12be4df8528b493cee9bf6
Giovanni Prinzivalli <Gprinziv@gmail.com>
2023-07-12 19:45:48 +0900

    Instead of flattening, update every dir together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def part1():
-    with open("test") as f:
+    with open("input") as f:
         cmds = [line.strip() for line in f.readlines()]
 
     dirs, i, cur = {}, 0, []
@@ -7,21 +7,23 @@ def part1():
     while i < len(cmds):
         cmd = cmds[i].split()
         if cmd[1] =="cd":
-            cur.pop() if cmd[2] == ".." else cur.append(cmd[2])
+            if cmd[2] == "..":
+                cur.pop()
+            else:
+                cur.append(cmd[2])
+                if cmd[2] not in dirs.keys():
+                    dirs[cmd[2]] = 0
             i += 1
         elif cmd[1] == "ls":
-            temp = [0]
             i += 1
             while i < len(cmds) and cmds[i][0] != "$":
                 cmd = cmds[i].split()
-                if cmd[0] == "dir":
-                    temp.append(cmd[1])
-                else:
-                    temp[0] += int(cmd[0])
+                if cmd[0] != "dir":
+                    for j in range(len(cur)):
+                        dirs[cur[j]] += int(cmd[0])
                 i += 1
-            dirs[cur[-1]] = temp
 
-    print(dirs)
-    #We've made our tree. Now how the hell do we solve for it efficiently?
+    total = sum([val for val in dirs.values() if val < 100000])
+    print(total)
 
 part1()
