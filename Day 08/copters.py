1f3b82708dfd885a342d4538dc26d6aa664e4d8a
Giovanni Prinzivalli <Gprinziv@gmail.com>
2023-10-12 15:47:24 +0900

    Day 8 finally done! I'm all "caught" up

--- Day 08/copters.py
+++ Day 08/copters.py
@@ -1,6 +1,7 @@
 from numpy import zeros
 from numpy import rot90
 from numpy import array
+import math
 
 def getTrees(fn):
     trees = []
@@ -9,34 +10,52 @@ def getTrees(fn):
             trees.append([int(i) for i in line.strip()])
     return trees
 
-
 def p1():
     trees = array(getTrees("input"))
-    t2 = rot90(trees,2)
-    t3 = zeros((len(trees[0]), len(trees)), int)
+    tmap = zeros((len(trees[0]), len(trees)), int)
 
+    t2 = rot90(trees, -1)
     for j in range(1, len(trees)-1):
-        lmax, rmax = trees[1][0], t2[1][0]
-        umax, dmax = trees[0][:], t2[0][:]
         for i in range(1, len(trees[0])-1):
-            if trees[j][i] > lmax:
-                lmax = trees[j][i]
-                t3[j][i] = 1
-            if trees[j][i] > umax[i]:
-                umax[i] = trees[j][i]
-                t3[j][i] = 1
-            if t2[j][i] > rmax:
-                rmax = t2[j][i]
-                t3[-1-j][-1-i] = 1
-            if t2[j][i] > dmax[i]:
-                dmax[i] = t2[j][i]
-                t3[-1-j][-1-i] = 1
-    return sum(sum(t3)) + 2 * (len(t3) + len(t3[0]) - 2)
+            if trees[j][i] > max(trees[j][:i]) or trees[j][i] > max(trees[j][i+1:]) or trees[j][i] > max(t2[i][-j:]) or trees[j][i] > max(t2[i][:-j-1]):
+                tmap[j][i] = 1
+                
+    #The forest is a perfect square so we can "simplify" to the length of a side - 1
+    return sum(sum(tmap)) + 4 * (len(tmap) - 1)
 
 def p2():
-    trees = getTrees("input")
-    total = len(trees) * len(trees[0])    
-    return total
+    trees = array(getTrees("input"))
+    maxVis = 0   
+    for j in range(1, len(trees)-1):
+        for i in range(1, len(trees[0])-1):
+            treeVis = [0, 0, 0, 0]
+            
+            for n in range(j-1, -1, -1):
+                treeVis[0] += 1
+                if trees[n][i] >= trees[j][i]:
+                    break
+
+            for n in range(j+1, len(trees)):
+                treeVis[1] += 1
+                if trees[n][i] >= trees[j][i]:
+                    break
+
+            for n in range(i-1, -1, -1):
+                treeVis[2] += 1
+                if trees[j][n] >= trees[j][i]:
+                    break
+
+            for n in range(i+1, len(trees[j])):
+                treeVis[3] += 1
+                if trees[j][n] >= trees[j][i]:
+                    break
+
+            visScore = math.prod(treeVis)
+            if visScore > maxVis:
+                maxVis = visScore
+                print(f"Array ({i+1},{j+1}): {treeVis}; product: {visScore}")
+
+    return maxVis
     
 print(p1())
 print(p2())
--- main.py
+++ main.py
@@ -1,32 +1,4 @@
-from numpy import zeros
-from numpy import rot90
-from numpy import array
+def main():
+    pass
 
-def getTrees(fn):
-    trees = []
-    with open(fn) as f:
-        for line in f.readlines():
-            trees.append([int(i) for i in line.strip()])
-    return trees
-
-def p1():
-    trees = array(getTrees("input"))
-    tmap = zeros((len(trees[0]), len(trees)), int)
-
-    t2 = rot90(trees, -1)
-    print(t2)
-    for j in range(1, len(trees)-1):
-        for i in range(1, len(trees[0])-1):
-            if trees[j][i] > max(trees[j][:i]) or trees[j][i] > max(trees[j][i+1:]) or trees[j][i] > max(t2[i][-j:]) or trees[j][i] > max(t2[i][:-j-1]):
-                tmap[j][i] = 1
-                
-    #The forest is a perfect square so we can "simplify" to the length of a side - 1
-    return sum(sum(tmap)) + 4 * (len(tmap) - 1)
-
-def p2():
-    trees = getTrees("input")
-    total = len(trees) * len(trees[0])    
-    return total
-    
-print(p1())
-print(p2())
+main()
