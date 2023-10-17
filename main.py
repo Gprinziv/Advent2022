a42b5cc08c5324383d417fa9fda34c840f90a57e
Giovanni Prinzivalli <Gprinziv@gmail.com>
2023-10-17 21:54:45 +0900

    ALmost done with the day!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 TEST = 10
+TEST2 = 21
 PART1 = 2000000
+PART2 = 4000001
 
 
 def main():
@@ -37,10 +39,40 @@ def main():
     #print(f"The empty set is {empty}")
     print(len(empty) - len(full))
 
-
+#Find the sdiff of each beacon, then in a nested for loop compare the distance to each beacon to that of the point.
+#Pros: Easy as hell to write.
+#Cons: Slow, slow as shit. Like multiple days to finish slow
 def main2():
-    with open("input") as f:
-        pass
+    with open("test") as f:
+        coords = []
+        for line in f.readlines():
+            coords.append([int(y) for y in re.split(",|:|=", line)[1::2]])
+    #I could probably have done this in the above equation but meh, this works.
+    #Get the sdiff for each beacon here.
+    for x in range(len(coords)):
+        coords[x] = coords[x] + [abs(coords[x][0] - coords[x][2]) + abs(coords[x][1] - coords[x][3])]
+    
+    """
+    for j in range(PART2):
+        for i in range(PART2):
+            isoFlag = True
+            for sen in coords:
+                if (abs(sen[0] - i) + abs(sen[1] - j)) <= sen[4]:
+                    isoFlag = False
+                    break
+            if isoFlag == True:
+                print(f"Good coords at ({i}, {j})")
+                return
+    """
+    for sen in coords:
+        print(f"Coords ({sen[0]}, {sen[1]}); sdiff: {sen[4]}")
+        for j in range(sen[1] - sen[4], sen[1] + sen[4] + 1):
+            i = sen[4] - (j-sen[1]) 
+            print(j)
+        #check every point on the perimeter and compare it to the distances of each other beacon.
+        #Start at the top, then go down-right, then go down-left, then go up-left, then go up-right.
+#Let's look at it another way. WE don't need to check every point, just the points that are 1 greater than the perimeter of each beacon.
 
-main()
+
+#main()
 main2()
